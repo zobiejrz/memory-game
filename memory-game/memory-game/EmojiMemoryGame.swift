872cb143c3824f78f1ab2e2bc8f9fdcab7909338
Ben Zobrist <ben@zobie.com>
2020-06-11 14:08:49 -0400

    Lectures 1 and 2
    Implemented code from Lecture 1: Course Logistics and Introduction to SwiftUI and Lecture 2: MVVM and the Swift Type System

---
+++ memory-game/memory-game/EmojiMemoryGame.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiMemoryGame.swift
+//  memory-game
+//
+//  Created by Ben Zobrist on 6/11/20.
+//  Copyright © 2020 Ben Zobrist. All rights reserved.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    private var model = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["🍎", "🍌", "🍊"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ memory-game/memory-game/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  memory-game
+//
+//  Created by Ben Zobrist on 6/11/20.
+//  Copyright © 2020 Ben Zobrist. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
